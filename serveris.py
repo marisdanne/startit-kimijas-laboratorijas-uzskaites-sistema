5c7814f52474e61b4ba43e4c2b0db6a8e953381c
Māris Danne <maris.danne@rvt.lv>
2020-12-29 09:20:42 +0000

    Izveidots route sveiki

--- serveris.py
+++ serveris.py
@@ -1,13 +1,18 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
-
+# Sakuma lapa
 @app.route('/')
 def index():
-    return "Labrīt!"
+  return "Labrīt!!!"
 
+@app.route('/sveiki')
+def sveiki():
+  return "Nav vairs nekāds rīts!"
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", debug=True)
+  app.run("0.0.0.0", debug=True)
+
+
+# python serveris.py
