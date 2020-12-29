d7af4e10f8211b4eb71abcc5fb21f7204c2f8814
Māris Danne <maris.danne@rvt.lv>
2020-12-29 10:12:11 +0000

    pievienots vielas api

--- serveris.py
+++ serveris.py
@@ -1,12 +1,28 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+import dati
 
 app = Flask(__name__)
 
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def index():
-    return "Labrīt!"
+  return "Labrīt!"
+
+@app.route('/api/v1/vielas')
+def vielas():
+  return jsonify(dati.vielas)
+
+@app.route('/api/v1/viela/<vielasID>')
+def viela(vielasID):
+
+  viela = f"Viela ar doto id {vielasID} nav atrasta"
+
+  for v in dati.vielas:
+    if str(v['id']) == vielasID:
+      viela = v
+
+  return jsonify(viela)
 
 
 if __name__ == "__main__":
