77950d899f3cf2d5210988dd36f1d603a109c98d
Māris Danne <maris.danne@rvt.lv>
2020-12-29 09:46:35 +0000

    Pievienots route cik (reizinajums)

--- serveris.py
+++ serveris.py
@@ -11,6 +11,18 @@ def index():
 def sveiki():
   return "Nav vairs nekāds rīts!"
 
+@app.route('/sveiki/<vards>')
+def sveikiPersona(vards):
+  return f"Sveiki {vards}" # "Sveiki {}".format(vards) #
+
+@app.route('/cik/<sk1>/<sk2>')
+def reizinajums(sk1, sk2):
+  sk1 = int(sk1)
+  sk2 = int(sk2)
+  reizinajums = sk1 * sk2
+  return str(reizinajums)
+
+
 if __name__ == "__main__":
   app.run("0.0.0.0", debug=True)
 
